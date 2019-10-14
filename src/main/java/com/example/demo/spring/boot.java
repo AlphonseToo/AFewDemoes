f9c3d0e7053c8bb0abdb8e4dae73d7b1d4491004
zhuyaf <zhuyafeng>
2019-10-14 11:00:12 +0800

    填充类

--- src/main/java/com/example/demo/spring/boot.java
+++ src/main/java/com/example/demo/spring/boot.java
@@ -1,5 +1,7 @@
 package com.example.demo.spring;
 
+import lombok.Data;
+
 /**
  * @ClassName: boot
  * @Description: TODO
@@ -7,6 +9,8 @@ package com.example.demo.spring;
  * @Version: 1.0
  * @Date: 2019/10/14 10:52
  **/
+@Data
 public class boot {
-
+    private String player;
+    private String level;
 }
