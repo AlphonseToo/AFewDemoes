9adde3f1cc5eedb4ffbccf385ebf9012fd8619c3
zhuyaf <zhuyafeng>
2019-10-17 09:29:11 +0800

    章节5.2.2

--- src/main/java/com/thinking/demo/chapter5/StaticInitialization.java
+++ src/main/java/com/thinking/demo/chapter5/StaticInitialization.java
@@ -54,3 +54,25 @@ public class StaticInitialization {
     static Table table = new Table();
     static Cupboard cupboard = new Cupboard();
 }
+
+class Flower{
+    int petalCount = 0;
+    String s = "initial value";
+    Flower(){
+        this("hi", 47);
+        System.out.println("default constructor (no args)");
+    }
+    Flower(int petals){
+        petalCount = petals;
+        System.out.println("Constructor w/ int arg only, petalCount= " + petalCount);
+    }
+    Flower(String s, int petals){
+        this(petals);
+        this.s = s;
+        System.out.println("String & int args");
+    }
+
+    public static void main(String[] args){
+        Flower x = new Flower();
+    }
+}
