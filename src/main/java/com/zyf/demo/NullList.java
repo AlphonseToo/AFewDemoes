c72797352fd53f734ffd88dfa327cee4b8fe85c5
zhuyaf <zhuyafeng>
2020-05-11 16:11:04 +0800

    demo

---
+++ src/main/java/com/zyf/demo/NullList.java
@@ -0,0 +1,20 @@
+package com.zyf.demo;
+
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author Alphonse
+ * @date 2020/4/28 16:16
+ * @since 1.0
+ **/
+public class NullList {
+    public static void main(String[] args) {
+        List<String> list = null;
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println(list.toString());
+    }
+}
--- src/main/java/com/zyf/demo/PasswordTest.java
+++ src/main/java/com/zyf/demo/PasswordTest.java
@@ -9,13 +9,13 @@ package com.zyf.demo;
  **/
 public class PasswordTest {
     public static void main(String[] args) {
-        String pattern = "^(?=.*[0-9].*)(?=.*[a-zA-Z].*).{6,32}$";
+        String pattern = "^(?![\\d]+$)(?![a-zA-Z]+$)(?![^\\da-zA-Z]+$).{6,32}$";
         /**
-         * ^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{6,20}$
+         * ^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{6,32}$
          * 数字+字母，数字+特殊字符，字母+特殊字符，数字+字母+特殊字符组合，而且不能是纯数字，纯字母，纯特殊字符
          */
         /**
-         * ^(?![\d]+$)(?![a-zA-Z]+$)(?![!#$%^&*]+$)[\da-zA-Z!#$%^&*]{6,20}$
+         * ^(?![\d]+$)(?![a-zA-Z]+$)(?![!#$%^&*]+$)[\da-zA-Z!#$%^&*]{6,32}$
          * 上面的正则里所说的特殊字符是除了数字，字母之外的所有字符
          * 如果要限定特殊字符，例如，特殊字符的范围为 !#$%^&*
          */
@@ -23,7 +23,7 @@ public class PasswordTest {
          * ^(?=.*[0-9].*)(?=.*[a-zA-Z].*)(?=.*[!@#$%^&*?].*).{6,32}$
          * 由6到32位数字、字母、!@#$%^&*?字符组成!
          */
-        String pwd = "zyafaaaaaa1";
+        String pwd = "272727@";
         if(pwd.matches(pattern)) {
             System.out.println("正确。");
         } else {
